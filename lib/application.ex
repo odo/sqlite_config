3619f54d3df28e4f45976cf0cdbfa3ff5a3cd2e0
Florian Odronitz <odo@mac.com>
2026-09-04 16:40:07 +0200

    no need for a config

--- lib/application.ex
+++ lib/application.ex
@@ -4,8 +4,8 @@ defmodule SqliteConfig.Application do
   @impl true
   def start(_, _) do
     children = [
-      SqliteConfig.Repo,
-      SqliteConfig.StageRepo,
+      {SqliteConfig.Repo, database: :memory, pool_size: 1},
+      {SqliteConfig.StageRepo, database: :memory, pool_size: 1},
     ]
     opts = [strategy: :one_for_one, name: SqliteConfig.Supervisor]
     Supervisor.start_link(children, opts)
