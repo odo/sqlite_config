1c35bcd04bb085d1c1920b1e2f1c3610bf6846af
Florian Odronitz <odo@mac.com>
2026-09-15 16:20:58 +0200

    getting ready for hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule SqliteConfig.MixProject do
       version: "0.1.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -23,6 +24,16 @@ defmodule SqliteConfig.MixProject do
   defp deps do
     [
       {:ecto_sqlite3, "~> 0.17"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+  
+  defp package() do
+    [
+     name: "sqlite_config",
+      links: %{"GitHub" => "https://github.com/odo/sqlite_config"},
+     description: "Dynamic configs in a SQLite DB",
+     licenses: ["MIT"],
     ]
   end
 end
